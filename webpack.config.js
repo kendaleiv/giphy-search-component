9bb27f5b317f294d54a5a7d39ab67a8810b599dd
Ken Dale <ken@kendaleiv.com>
2018-02-21 11:03:59 -0500

    Add analyze script

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Visualizer = require('webpack-visualizer-plugin');
 
 let plugins = [];
 
 if (process.env.INCLUDE_WEBPACK_HTML) {
   plugins.push(new HtmlWebpackPlugin({
     template: path.join(__dirname, 'index.html')
+  }));
+}
+
+if (process.env.INCLUDE_WEBPACK_VISUALIZER) {
+  plugins.push(new Visualizer({
+    filename: '../stats.html'
   }));
 }
 
